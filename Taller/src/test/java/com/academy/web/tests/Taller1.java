9035ac7e290656492c186bad8f8ce7607a2195e2
Marcela <edith.lasso@globant.com>
2020-03-29 18:27:21 -0500

    Actualización

--- Taller/src/test/java/com/academy/web/tests/Taller1.java
+++ Taller/src/test/java/com/academy/web/tests/Taller1.java
@@ -1,43 +1,35 @@
 package com.academy.web.tests;
 
 import org.apache.log4j.Logger;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-@Test
 public class Taller1 {
 
     public Logger log = Logger.getLogger(Taller1.class);
 
     @BeforeSuite(groups = {"group1"})
     public void before () {
-        log.info("Entrar a la página. Debe Tener Usuario");
+        log.info("Entrar a la página");
     }
     
-    @BeforeMethod(groups = {"grouptest"})
+    @Test(groups = {"group2"},priority=2)
     public void iniciarSesion () {
         log.info("Iniciar Sesion");
     }
-
-    @BeforeMethod(groups = {"group2"})
-    public void verificarCuenta () {
-        log.info("Verificar que la cuenta existe");
-    }
-
+    
     @Test(groups = {"group1"}, priority=1)
     public void crearCuenta () {
         log.info("Crear Cuenta");
     }
-
-    @Test(groups = {"group2"}, priority=2)
+    
+    @Test(groups = {"group3"}, priority=3)
     public void cancelarCuenta () {
         log.info("Cancelar Cuenta");
     }
-
-    
-    @AfterMethod(groups = {"grouptest"})
+ 
+    @AfterTest(groups = {"group1", "group2"})
     public void cerrarSesion () {
         log.info("Cerrar Sesión");
     }
